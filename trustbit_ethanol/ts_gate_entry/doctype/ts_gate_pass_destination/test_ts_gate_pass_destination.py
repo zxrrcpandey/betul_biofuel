49984a932dd56a835b3c2ca20cfbc87556b185f0
Trustbit Deploy <dev@trustbit.in>
2026-04-01 05:20:32 +0000

    Add Admin Reception permission to TS Gate Pass Destination + boilerplate files (server sync)

---
+++ trustbit_ethanol/ts_gate_entry/doctype/ts_gate_pass_destination/test_ts_gate_pass_destination.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Trustbit Software and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTSGatePassDestination(FrappeTestCase):
+	pass
---
+++ trustbit_ethanol/ts_gate_entry/doctype/ts_gate_pass_destination/ts_gate_pass_destination.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Trustbit Software and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("TS Gate Pass Destination", {
+// 	refresh(frm) {
+
+// 	},
+// });
